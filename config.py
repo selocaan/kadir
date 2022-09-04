d59a32c00923880697932e7be7e89ade1ec68367
selocaan <112822749+selocaan@users.noreply.github.com>
2022-09-04 22:10:01 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,88 @@
+BOT_USERNAME = "MedyaScrapeBot"
+BOT_TOKEN = "5383608817:AAEQ_09aJzyGG68Xc9_gKhDNBNEHI-3goLM"
+
+OWNER_ID = [1924880157,5001825804,947148402,5207201048]
+
+CMD_START = ["start"]
+CMD_STOP = ["stop"]
+CMD_LOG = ["log"]
+CMD_CLEAR = ["clear"]
+
+#BUTTON FOR '/start' MENU
+BUTTON_ACCOUNT = "Hesaplar"
+BUTTON_ADDER = "Grup Ekleme"
+BUTTON_CLOSE = "Kapat"
+
+#BUTTON FOR 'Accounts' MENU
+BUTTON_ADD_ACCOUNT = "Hesap eklemek"
+BUTTON_DEL_ACCOUNT = "Hesap Silmek"
+BUTTON_BAN_FILTER = "Yasaklama Filtresi"
+
+#BUTTON FOR 'Adder' MENU
+BUTTON_PUBLIC = "Genel Gruba Ekle"
+BUTTON_PRIVATE = "Add to Private Group"
+
+BACK_BUTTON = "Geri"
+TEXT_HELPER = "**HELPER**"
+TEXT_ACCOUNT_MENU = "**ACCOUNT MENU**"
+TEXT_ADDER_MENU = "**ADDER MENU**"
+
+#TEXT FOR '/log' COMMAND
+TEXT_LOG_CAPTION = "Dev: @TURANMEDYAX"
+TEXT_LOG_TRYLATER = "Try later!"
+TEXT_LOG_TITLE = "[ LOG ]"
+TEXT_LOG_JGTS = "Joined group to scrape"
+TEXT_LOG_JGTA = "Joined group to add"
+TEXT_LOG_FTJG = "Failed to join group"
+TEXT_LOG_RTENGE = "Retrieving entities..."
+TEXT_LOG_FTSM = "Couldn't scrape members"
+TEXT_LOG_SS = "Starting session... "
+TEXT_LOG_STOPED = "Adder Member stopped!"
+TEXT_LOG_SLEEP = "Sleep {} sec" # DON'T REMOVE `{}` OR YOU WILL GET ERROR
+TEXT_LOG_SLPA = "Sleep {} second(s)" # DON'T REMOVE `{}` OR YOU WILL GET ERROR
+TEXT_LOG_NMTA = "No members to add!"
+TEXT_LOG_SA = "Starting adder..."
+TEXT_LOG_TMPFE = "Too many Peer Flood Errors! Closing session..."
+TEXT_LOG_SE = "Adding session ended"
+
+#TEXT FOR '/clear' COMMAND
+TEXT_CLEAR_SUCCESS = "<b>All accounts deleted!</b>"
+TEXT_CLEAR_FAILED = "<b>No available account to delete!</b>"
+TEXT_CLEAR_RUNNING = "<b>Adder Member</b> is running!\nstop this first use command /stop"
+
+#TEXT FOR '/stop' COMMAND
+TEXT_STOP_SUCCESS = "<b>Adder Member</b> stopped!"
+TEXT_STOP_FAILED = "<b>Adder Member</b> already not running!"
+
+
+TEXT_A_SENDPHONE = "Send phone number!"
+TEXT_A_LOGGINGNEW = "[+] Logging in from new accounts"
+TEXT_A_LOGGINGFROM = "[*] Logging in from "
+TEXT_A_ENTERCODE = "Enter code : "
+TEXT_A_ENTERPASSWORD = "Enter password : "
+TEXT_A_SUCCESS = "[+] Login successful"
+TEXT_A_FAILEDCODE = "[!] Invalid code. Please try again..."
+TEXT_A_FAILEDPASSWORD = "[!] Invalid password. Please try again..."
+TEXT_A_FAILEDEXPIRED = "[!] Code is expired. Please try again..."
+
+TEXT_C_RUNNING = "Failed to start <b>Adder Member</b> already running. Command /stop to stop the current <b>Adder Member</b>"
+TEXT_C_CHOOSE = "Total accounts: {}\nEnter number of accounts to use" # DON'T REMOVE `{}` OR YOU WILL GET ERROR
+
+TEXT_D_CHOOSE = "**Choose an account to delete**"
+TEXT_D_SUCCESS = "[!] Account Deleted"
+
+TEXT_F_FILTERING = "**Filtering All Banned Account**"
+TEXT_F_ACCOUNTEMPTY = "[!] There are no accounts! Please add some and retry"
+TEXT_F_NOBANNED = "Congrats! No banned accounts"
+TEXT_F_SUCCESS = "All Banned Accounts Removed"
+
+TEXT_U_RESUME = "Resume scraping members from "
+TEXT_U_YES = "YES"
+TEXT_U_NO = "NO"
+TEXT_U_SENDURL = 'Send Public/Private group link to scrape members'
+
+TEXT_R_DELAY = "Enter delay time per request 0 for None"
+TEXT_R_CHOOSE = "Choose an option"
+TEXT_R_PUBLIC = "Send public group link"
+TEXT_R_PRIVATE = "Send private group link"
+TEXT_R_ADDING = "Adding members from {} account(s)\ncommand /log to see log info" # DON'T REMOVE `{}` OR YOU WILL GET ERROR
